20d19ee0c09516f348f1f2421f238b2aa2dfd27c
syifacts <ss4166269@gmail.com>
2024-12-14 14:12:18 +0700

    perbaikan halaman aboutus

--- src/scripts/views/pages/aboutus.js
+++ src/scripts/views/pages/aboutus.js
@@ -8,39 +8,39 @@ const aboutus = {
                     <h3>Parlaungan Siregar</h3>
                     <p>Universitas Malikussaleh</p>
                     <p>
-                        <a href="https://github.com/Parlaungan08"><img src="assets/github.png" alt="Github"></a>
-                        <a href="https://www.linkedin.com/in/parlaungan-siregar-1493322b9/"><img src="assets/linkedid.png" alt="Linkedid"></a>
-                        <a href="https://www.instagram.com/parlaungan344/"><img src="assets/1384063.png" alt="instagram"></a>
+                        <a href="https://github.com/Parlaungan08" target="_blank"><img src="assets/github.png" alt="Github"></a>
+                        <a href="https://www.linkedin.com/in/parlaungan-siregar-1493322b9/" target="_blank"><img src="assets/linkedid.png" alt="Linkedin"></a>
+                        <a href="https://www.instagram.com/parlaungan344/" target="_blank"><img src="assets/1384063.png" alt="Instagram"></a>
                     </p>
                 </div>
                 <div class="team-member">
-                    <img src="assets/fadil.jpg" alt="fadil">
+                    <img src="assets/fadil.jpg" alt="Ikhsan Fadillah">
                     <h3>Ikhsan Fadillah</h3>
-                    <p>Politeknik Negeri Jakarta </p>
+                    <p>Politeknik Negeri Jakarta</p>
                     <p>
-                        <a href="https://github.com/ikhsan111"><img src="assets/github.png" alt="Github"></a>
-                        <a href="https://www.linkedin.com/in/parlaungan-siregar-1493322b9/"><img src="assets/linkedid.png" alt="Linkedid"></a>
-                        <a href="https://www.instagram.com/iksanfdllh/"><img src="assets/1384063.png" alt="instagram"></a>
+                        <a href="https://github.com/ikhsan111" target="_blank"><img src="assets/github.png" alt="Github"></a>
+                        <a href="https://www.linkedin.com/in/parlaungan-siregar-1493322b9/" target="_blank"><img src="assets/linkedid.png" alt="Linkedin"></a>
+                        <a href="https://www.instagram.com/iksanfdllh/" target="_blank"><img src="assets/1384063.png" alt="Instagram"></a>
                     </p>
                 </div>
                 <div class="team-member">
-                    <img src="assets/sifa.jpg" alt="sifa">
+                    <img src="assets/sifa.jpg" alt="Syifa Chandra Tiffani">
                     <h3>Syifa Chandra Tiffani</h3>
-                    <p>Politeknik Negeri Jakarta </p>
+                    <p>Politeknik Negeri Jakarta</p>
                     <p>
-                        <a href="https://github.com/syifacts"><img src="assets/github.png" alt="Github"></a>
-                        <a href="https://www.linkedin.com/in/syifa-chandra-tiffani-sumardi/"><img src="assets/linkedid.png" alt="Linkedid"></a>
-                        <a href="https://www.instagram.com/syifacts_?igsh=bGVibjRxbnU3OWtl "><img src="assets/1384063.png" alt="instagram"></a>
+                        <a href="https://github.com/syifacts" target="_blank"><img src="assets/github.png" alt="Github"></a>
+                        <a href="https://www.linkedin.com/in/syifa-chandra-tiffani-sumardi/" target="_blank"><img src="assets/linkedid.png" alt="Linkedin"></a>
+                        <a href="https://www.instagram.com/syifacts_?igsh=bGVibjRxbnU3OWtl" target="_blank"><img src="assets/1384063.png" alt="Instagram"></a>
                     </p>
                 </div>
                 <div class="team-member">
-                    <img src="assets/yeudanta.jpg" alt="sifa">
+                    <img src="assets/yeudanta.jpg" alt="Yeudanta Mahardika">
                     <h3>Yeudanta Mahardika</h3>
-                    <p>Universitas Duta Wacana </p>
+                    <p>Universitas Duta Wacana</p>
                     <p>
-                        <a href="https://github.com/yuudantaa"><img src="/assets/github.png" alt="Github"></a>
-                        <a href="https://id.linkedin.com/in/yeudanta-mahardika-aditya-wicaksana-87222733b"><img src="assets/linkedid.png" alt="Linkedid"></a>
-                        <a href="https://www.instagram.com/yu_dantaa_/  "><img src="assets/1384063.png" alt="instagram"></a>
+                        <a href="https://github.com/yuudantaa" target="_blank"><img src="assets/github.png" alt="Github"></a>
+                        <a href="https://id.linkedin.com/in/yeudanta-mahardika-aditya-wicaksana-87222733b" target="_blank"><img src="assets/linkedid.png" alt="Linkedin"></a>
+                        <a href="https://www.instagram.com/yu_dantaa_/" target="_blank"><img src="assets/1384063.png" alt="Instagram"></a>
                     </p>
                 </div>
             </div>
